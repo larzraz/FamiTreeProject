45fadfd7d44d20017c43b77b4cf9559e2ff05803
Mikkel Steenberg Kristoffersen <mikk6053@edu.eal.dk>
2018-11-28 14:37:52 +0100

    lavet LoginViewModel & lavet en login page

---
+++ FamiTreeProject/Models/LoginViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace FamiTreeProject.Models
+{
+    public class LoginViewModel
+    {
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }
+    }
+}
