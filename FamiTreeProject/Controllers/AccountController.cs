3c7f6d68cd1e1b48e8103df3aa5b67069ff80094
Mikkel Steenberg Kristoffersen <mikk6053@edu.eal.dk>
2018-12-19 16:14:37 +0100

    as

--- FamiTreeProject/Controllers/AccountController.cs
+++ FamiTreeProject/Controllers/AccountController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using FamiTreeProject.Models;
+using FamiTreeProject.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
